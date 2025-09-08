977903062a3a55d3368d32fb90d0c13c3f6ee316
pahmatug81042 <ptug81042@yahoo.com>
2025-09-08 14:30:21 +0000

    commented blocks of code

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ connectDB();
 
 app.use(express.json());
 
-// Serve React frontend build
-app.use(express.static(path.join(__dirname, 'client/build')));
+/* // Serve React frontend build
+app.use(express.static(path.join(__dirname, 'client/build'))); */
 
 // API routes
 app.use('/api/tasks', routeTasks);
 
-// For client-side routing: serve React index.html on all other requests
+/* // For client-side routing: serve React index.html on all other requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+}); */
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
